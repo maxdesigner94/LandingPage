5ef5683b94ca068ca8eecd590ad789b2dcca53c5
maxdesigner94 <144882359+maxdesigner94@users.noreply.github.com>
2025-11-20 23:25:29 +0100

    Refactor root rendering logic for safety

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+}
+
