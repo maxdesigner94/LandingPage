6abe03b5ef9a4826f00530fe17bd227ef613a931
maxdesigner94 <144882359+maxdesigner94@users.noreply.github.com>
2025-11-21 00:36:57 +0100

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Footer(){
+export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
       <div className="container">
@@ -8,9 +8,7 @@ export default function Footer(){
           <strong>FlashImpianti</strong>
           <div className="small">Impianti elettrici civili e industriali • Progettazione e manutenzione</div>
         </div>
-        <div className="small">
-          Email: <a href="mailto:info@flashimpianti.it">info@flashimpianti.it</a> • Tel: <a href="tel:+39080000000">+39 080 000000</a>
-        </div>
+        <div className="small">Email: <a href="mailto:info@flashimpianti.it" style={{color:"inherit", textDecoration:"underline"}}>info@flashimpianti.it</a> • Tel: <a href="tel:+39080000000" style={{color:"inherit", textDecoration:"underline"}}>+39 080 000000</a></div>
       </div>
     </footer>
   );
